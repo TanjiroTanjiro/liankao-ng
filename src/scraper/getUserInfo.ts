d04d2a5837178449959a589b84a50f7eaea5c94a
TanjiroTanjiro <64674261+TanjiroTanjiro@users.noreply.github.com>
2026-04-03 22:45:10 +0800

    Changed getUserInfo

--- src/scraper/getUserInfo.ts
+++ src/scraper/getUserInfo.ts
@@ -1,7 +1,7 @@
 import * as cheerio from 'cheerio';
 import { fetchHtml } from './fetch';  
 
-export async function getUserRealname(xstoken: string) {
+export async function getUserRealname(xstoken: string){
     const webInfo=await fetchHtml("http://xsy.gdgzez.com.cn/JudgeOnline/modifypage.php",xstoken);
     const nameSelector = '#wrapper > div.form-container > form > div:nth-child(5) > p:nth-child(3)';
     const $ = cheerio.load(webInfo);
@@ -10,4 +10,12 @@ export async function getUserRealname(xstoken: string) {
     // console.log(realname);
     if(realname.length == 0)    throw Error("Failed to get name");
     return realname;
+}
+export async function getUserXsyName(xstoken: string){
+    const webInfo=await fetchHtml("http://xsy.gdgzez.com.cn/JudgeOnline/modifypage.php",xstoken);
+    const nameSelector = '#wrapper > div.form-container > form > div:nth-child(4) > p';
+    const $ = cheerio.load(webInfo);
+    const xsyname=$(nameSelector).text().trim();
+    if(xsyname.length == 0)    throw Error("Failed to get name");
+    return xsyname;
 }
