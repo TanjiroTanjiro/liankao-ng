82bd7e41d10d503f82480ad2cd833578ea4fd072
ETO_leader <cycmail001@gmail.com>
2026-04-06 17:28:42 +0800

    增加 api views

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Elysia } from "elysia";
 import { auth } from "./modules/auth";
+import { view } from "./modules/view"
 import openapi from "@elysiajs/openapi";
+import prisma from "./prisma";
 
-const app = new Elysia().use(auth).use(openapi()).listen(3000);
+const app = new Elysia()
+    .use(auth)
+    .use(view)
+    .use(openapi())
+    .get('/status', () => {
+        return {message: 'API ready'}
+    })
+    .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+
