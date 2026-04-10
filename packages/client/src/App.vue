bbeac65db6b6a4a615dbd96def0ab2cb7f85e3ad
TanjiroTanjiro <64674261+TanjiroTanjiro@users.noreply.github.com>
2026-04-10 16:04:18 +0800

    Added VUE

---
+++ packages/client/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="container">
+    <h1>Vue 测试页面</h1>
+    <p>qnq</p>
+    <button @click="count++">当前计数：{{ count }}</button>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
